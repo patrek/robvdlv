82c3c3ce8d25c04dff21523bdd9c5602a378765b
erik.pragt@gmail.com <erik.pragt@gmail.com@9ba06fb4-2f4f-11de-ba90-e1c2dc4d389a>
2009-11-28 15:37:50 +0000

    Added Event Handler

--- trunk/cqrs/src/main/java/nl/ddd/event/CatalogusCreatedEvent.java
+++ trunk/cqrs/src/main/java/nl/ddd/event/CatalogusCreatedEvent.java
@@ -1,5 +1,7 @@
 package nl.ddd.event;
 
+import java.util.UUID;
+
 import nl.ddd.eventstorage.Event;
 
 /**
@@ -8,4 +10,13 @@ import nl.ddd.eventstorage.Event;
  * @author Rob van der Linden Vooren
  */
 public class CatalogusCreatedEvent extends Event {
+    private UUID uuid;
+
+    public CatalogusCreatedEvent(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
 }
--- trunk/cqrs/src/main/java/nl/ddd/eventstorage/Event.java
+++ trunk/cqrs/src/main/java/nl/ddd/eventstorage/Event.java
@@ -7,5 +7,4 @@ import java.io.Serializable;
  */
 public abstract class Event implements Serializable {
 
-
 }
